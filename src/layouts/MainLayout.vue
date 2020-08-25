3f6c3a71216f8b7d73ec77134cb891170ae75824
midiblocks <midiblocks@gmail.com>
2020-08-25 13:53:01 -0700

    #2 Adds a splitter layout for code area and console

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,18 +1,39 @@
 <template lang="pug">
-q-layout(view='lHh Lpr lFf')
+q-layout.full-height(view='lHh Lpr lFf')
   q-header(elevated='')
     q-toolbar
       q-btn(flat='' dense='' round='' icon='menu' aria-label='Menu' @click='leftDrawerOpen = !leftDrawerOpen')
-        q-toolbar-title MIDIBlocks
-        div Quasar v{{ $q.version }}
+      q-toolbar-title MIDIBlocks
+      div Quasar v{{ $q.version }}
   q-drawer(v-model='leftDrawerOpen' show-if-above='' bordered='' content-class='bg-grey-1')
-  q-page-container
+  q-page-container.full-height
     router-view
 </template>
 
 <script>
 export default {
   name: 'MainLayout',
+
+  watch: {
+    /**
+     * Resize the Blockly editor
+     */
+    leftDrawerOpen () {
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'))
+      }, 50)
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'))
+      }, 100)
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'))
+      }, 150)
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'))
+      }, 200)
+    }
+  },
+  
   data () {
     return {
       leftDrawerOpen: false
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,36 @@
 <template lang="pug">
-q-page
-  blockly.blockly(ref='foo' :options='options')
-    category(name='Logic' colour='%{BKY_LOGIC_HUE}')
-      block(type='controls_if')
-      block(type='logic_compare')
-      block(type='logic_operation')
-      block(type='logic_negate')
-      block(type='logic_boolean')
-    category(name='Loops' colour='%{BKY_LOOPS_HUE}')
-      block(type='controls_repeat_ext')
-        value(name='TIMES')
+q-page.full-height
+  q-splitter.full-height(v-model='splitter' horizontal unit='px' reverse)
+    template(v-slot:after)
+      div(v-html='log' style="padding: 20px;")
+    template(v-slot:before)
+      blockly.blockly(ref='foo' :options='options')
+        category(name='Logic' colour='%{BKY_LOGIC_HUE}')
+          block(type='controls_if')
+          block(type='logic_compare')
+          block(type='logic_operation')
+          block(type='logic_negate')
+          block(type='logic_boolean')
+        category(name='Loops' colour='%{BKY_LOOPS_HUE}')
+          block(type='controls_repeat_ext')
+            value(name='TIMES')
+              block(type='math_number')
+                field(name='NUM') 10
+          block(type='controls_whileUntil')
+        category(name='Math' colour='%{BKY_MATH_HUE}')
           block(type='math_number')
-            field(name='NUM') 10
-      block(type='controls_whileUntil')
-    category(name='Math' colour='%{BKY_MATH_HUE}')
-      block(type='math_number')
-        field(name='NUM') 123
-      block(type='math_arithmetic')
-      block(type='math_single')
-    category(name='Text' colour='%{BKY_TEXTS_HUE}')
-      block(type='text')
-      block(type='text_length')
-      block(type='text_print')
-    category(name='Variables' custom='VARIABLE' colour='%{BKY_VARIABLES_HUE}')
-    category(name='Stocks' colour='%{BKY_LOOPS_HUE}')
-      block(type='stock_buy_simple')
-      block(type='stock_buy_prog')
-      block(type='stock_fetch_price')
-  p#code
-    button(v-on:click='showCode()') Show JavaScript
-    pre(v-html='code').
+            field(name='NUM') 123
+          block(type='math_arithmetic')
+          block(type='math_single')
+        category(name='Text' colour='%{BKY_TEXTS_HUE}')
+          block(type='text')
+          block(type='text_length')
+          block(type='text_print')
+        category(name='Variables' custom='VARIABLE' colour='%{BKY_VARIABLES_HUE}')
+        category(name='Stocks' colour='%{BKY_LOOPS_HUE}')
+          block(type='stock_buy_simple')
+          block(type='stock_buy_prog')
+          block(type='stock_fetch_price')
 </template>
 
 <script>
@@ -45,8 +46,15 @@ export default {
     Blockly
   },
 
+  watch: {
+    splitter () {
+      window.dispatchEvent(new Event('resize'))
+    }
+  },
+
   data () {
     return {
+      log: 'Welcome to MIDIBlocks 🎹',
       code: '',
       options: {
         media: 'media/',
@@ -56,13 +64,8 @@ export default {
           colour: '#ccc',
           snap: true
         }
-      }
-    }
-  },
-
-  methods: {
-    showCode () {
-      this.code = BlocklyJS.workspaceToCode(this.$refs.foo.workspace)
+      },
+      splitter: 100
     }
   }
 }
