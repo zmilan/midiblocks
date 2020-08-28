29f9ecaaa7f5c9352bc4b942d4898ae0e155dffb
midiblocks <midiblocks@gmail.com>
2020-08-28 12:05:15 -0700

    comitting code to switch to another computer

--- src/blocks/midi-events.js
+++ src/blocks/midi-events.js
@@ -2,11 +2,23 @@ import * as Blockly from 'blockly/core'
 
 Blockly.JavaScript['when_midi_event'] = function(block) {
   const dropdown_event = block.getFieldValue('event');
-var code = `$m.on('${dropdown_event}', function (note) {
+let code = `$m.on('${dropdown_event}', function (note) {
   log(note)
 });\n`;
   return code;
 };
+
+Blockly.JavaScript['midi_send_note'] = function(block) {
+  let text_note = block.getFieldValue('note');
+  let text_channel = block.getFieldValue('channel');
+  let value_meta = Blockly.JavaScript.valueToCode(block, 'meta', Blockly.JavaScript.ORDER_ATOMIC);
+
+  let code = `$m('note ${text_note}').play({
+  channel: ${text_channel}
+})`
+
+  return code
+}
 
 /**
  * 👇👇👇 DO NOT EDIT BLOCK DEFINITIONS BELOW MANUALLY 👇👇👇
@@ -18,13 +30,29 @@ var code = `$m.on('${dropdown_event}', function (note) {
 Blockly.Blocks['when_midi_event'] = {
   init: function() {
     this.appendDummyInput()
-      .appendField('When 🎹')
+        .appendField("When 🎹");
     this.appendDummyInput()
-      .appendField(new Blockly.FieldDropdown([['Note On','noteon'], ['Note Off','noteoff'], ['Control Change','controlchange']]), 'event')
-    this.setInputsInline(true)
-    this.setNextStatement(true, null)
-    this.setColour(60)
-    this.setTooltip('')
-    this.setHelpUrl('')
+        .appendField(new Blockly.FieldDropdown([["Note On","noteon"], ["Note Off","noteoff"], ["Control Change","controlchange"]]), "event");
+    this.setInputsInline(true);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+ this.setTooltip("");
+ this.setHelpUrl("");
   }
-}
+};
+
+Blockly.Blocks['midi_send_note'] = {
+  init: function() {
+    this.appendValueInput("meta")
+        .setCheck("String")
+        .appendField("Play Note")
+        .appendField(new Blockly.FieldTextInput("C"), "note")
+        .appendField("on channel")
+        .appendField(new Blockly.FieldTextInput("all"), "channel");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(60);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
