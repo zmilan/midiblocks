473013d9909dbe4e0dbd6653a3b8b7bd2eb56ee4
Oz Ramos <midiblocks@gmail.com>
2020-08-30 19:05:33 -0700

    #20 Updates favicon, adds console message

--- src/boot/main.js
+++ src/boot/main.js
@@ -4,4 +4,26 @@ import pkg from '../../package.json'
 // Ignored elements for Blockly
 Vue.config.ignoredElements = ['field','block','category','xml','mutation','value','sep']
 
+/**
+ * Center a line of ASCII given some length
+ */
+const centerText = function (text, lineLength) {
+  const padLength = (lineLength + text.length) / 2
+  let str = text.padEnd(padLength, ' ')
+    .padStart(lineLength, ' ')
+  
+  return str
+}
+
+/**
+ * Console message
+ * @ss https://ascii.co.uk/art/puzzle
+ */
+console.log(`,-------.                    .----.
+|     __/                  __/    |
+|    (_${centerText('🎹 Midiblocks', 18)}(_       |
+|    _/${centerText(pkg.version, 19)}_/      |
+|   (                    (        |
+\`----\`                    \`-------'`)
+
 Vue.prototype.$version = pkg.version
