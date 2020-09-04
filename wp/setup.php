5634a4471e541e3fb41cde6a50ddcc58a27f4a3b
Oz Ramos <midiblocks@gmail.com>
2020-09-04 16:01:02 -0700

    #12 White labels login page

--- wp/setup.php
+++ wp/setup.php
@@ -9,3 +9,25 @@ add_action('after_setup_theme', function () {
     'mainNavPanel' => 'Main Nav Panel'
   ]);
 });
+
+/**
+ * White label
+ */
+add_filter('login_headerurl', function () {return home_url();});
+add_action('login_enqueue_scripts', function () { ?>
+  <style type="text/css">
+      #login h1 a, .login h1 a {
+        background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/dist/spa/favicon.png);
+        height: 65px;
+        width: auto;
+        background-size: fit;
+        background-repeat: no-repeat;
+        padding-bottom: 30px;
+      }
+      #login h1 a, .login h1:after {
+        content: 'Registration is closed.';
+        display: block;
+        color: red;
+      }
+  </style>
+<?php } );
