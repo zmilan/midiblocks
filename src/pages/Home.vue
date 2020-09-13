b03c238bcdf6274e1c4d9c9db862ac44a297728b
Oz Ramos <midiblocks@gmail.com>
2020-09-12 19:16:58 -0700

    #33 Makes home logo responsive

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   q-page
     section.content.flex.flex-center
-      div
-        q-card(style='width: 550px; margin: auto')
+      div.max-width-100
+        q-card.max-width-100(style='width: 550px; margin: auto')
           q-card-section.text-center
-            img.q-mt-lg(src='~assets/logo-title.png' style="width: 500px; max-width: 100%")
+            img.q-mt-lg.max-width-100(src='~assets/logo-title.png' style="width: 500px;")
             p.text-subtitle2.q-mt-lg A programmable MIDI Platform
             p.text-subtitle1 coming soon
             p
