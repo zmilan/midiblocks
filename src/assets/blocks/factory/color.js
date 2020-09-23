c2c155181e175413d8499e11853719d13369e61c
Oz Ramos <midiblocks@gmail.com>
2020-09-23 00:03:34 -0700

    #58 Set block color with quasar color picker

--- src/assets/blocks/factory/color.js
+++ src/assets/blocks/factory/color.js
@@ -4,16 +4,27 @@ import Blockly from 'blockly'
  * A color box with a quasar color picker
  */
 Blockly.Blocks['style_color'] = {
-  // Set the colour of the block
+  /**
+   * Sets the block's color
+   */
   init: function() {
     this.setColour('#fad000')
-    
     this.appendDummyInput()
         .appendField('color:')
         .appendField(new Blockly.FieldTextInput('#fad000', this.setColor), 'COLOR')
     this.setOutput(true, 'Color')
+
+    // @todo Update these
     this.setTooltip('Paint the block with this color.')
     this.setHelpUrl('https://www.youtube.com/watch?v=s2_xaEvcVI0#t=55')
+
+    // Listeners
+    setTimeout(() => {
+      // @TODO also handle focus/tabstop
+      this.getField('COLOR').getClickTarget_().addEventListener('click', () => {
+        document.dispatchEvent(new CustomEvent('blockly.showColorPicker', {detail: {block: this}}))
+      })
+    })
   },
 
   // Update the current block's colour to match
--- src/pages/FactoryHome.vue
+++ src/pages/FactoryHome.vue
@@ -6,6 +6,8 @@ q-page.full-height
         #preview(style='flex: 0 1 250px')
         CodeEditor(@onCodeChange='onCodeChange' :prefix='code.prefix' :value='code.generated')
     template(v-slot:before)
+      q-dialog(v-model='colorPicker.isVisible')
+        q-color(v-model='colorPicker.color' style='max-width: 250px' no-header no-footer default-view='palette' :palette='colorPicker.palette' @change='onColorPickerChange')
       Workspace.fill(ref='workspace' :toolbox='toolbox' :blocks='[]' :options='options' @change='workspaceEventHandler')
 </template>
 
@@ -36,15 +38,18 @@ export default {
     }, 50, {leading: true, trailing: true})
   },
 
-  computed: {
-
-  },
-
   data () {
     const currentFactory = store.get('currentFactory', {})
     
     return {
       hasLoaded: false,
+
+      colorPicker: {
+        color: '#fad000',
+        isVisible: false,
+        palette: ['#ff628c', '#FF9D00', '#fad000', '#2ca300', '#2EC4B6', '#5D37F0'],
+        block: null
+      },
       
       code: {
         // The code generated from the factory
@@ -90,9 +95,34 @@ export default {
     }
 
     this.$refs.workspace.blockly.addChangeListener(Blockly.Events.disableOrphans)
+    document.addEventListener('blockly.showColorPicker', this.showColorPicker)
+  },
+
+  destroyed () {
+    document.removeEventListener('blockly.showColorPicker', this.showColorPicker)
   },
 
   methods: {
+    /**
+     * Show the color picker
+     * @todo Move into component
+     */
+    showColorPicker (ev) {
+      this.colorPicker.color = '#000'
+      this.colorPicker.isVisible = true
+      this.colorPicker.block = ev.detail.block
+    },
+
+    /**
+     * Update the color picker
+     * @todo Move into component
+     */
+    onColorPickerChange (color) {
+      this.colorPicker.isVisible = false
+      this.colorPicker.block.getField('COLOR').setValue(color)
+      this.colorPicker.block.setFieldValue(color, 'COLOR')
+    },
+    
     /**
      * Autosave code to localstorage
      */
