dd8a32ae0fac1f45e9d0f691aba710c06e4a3283
Midiblocks <midiblocks@gmail.com>
2020-10-21 15:27:08 -0700

    #96 Drag blocks on Workspace

--- src/components/Workspace.vue
+++ src/components/Workspace.vue
@@ -68,6 +68,19 @@ export default {
       isFlyoutOpen: false,
       blockBeingDragged: false,
 
+      // Handsfree data
+      handsfree: {
+        // The BlocklyDragger object
+        // @see https://developers.google.com/blockly/reference/js/Blockly.BlockDragger#startBlockDrag
+        dragger: null,
+        
+        // Used for calculating drag deltas
+        orig: {
+          x: 0,
+          y: 0
+        }
+      },
+
       // Useful for re-showing a category toolbox (eg, after creating a variable)
       lastClickedCategory: null,
       isMouseInQuasarToolbox: false,
@@ -111,11 +124,13 @@ export default {
 
     // Handsfree
     document.addEventListener('handsfree-blockly-click', this.onBlocklyHandsfreeClick)
+    document.addEventListener('handsfree-data', this.onHandsfree)
   },
 
   beforeDestroy () {
     this.$root.$off('blockly.prompt.submit', this.onPromptSubmit)
     document.removeEventListener('handsfree-blockly-click', this.onBlocklyHandsfreeClick)
+    document.removeEventListener('handsfree-data', this.onHandsfree)
   },
 
   methods: {
@@ -164,13 +179,31 @@ export default {
      * Handle clicking on elements handsfree
      */
     onBlocklyHandsfreeClick (ev) {
+      if (this.handsfree.dragger) return
+      
       const $block = this.blockly.getBlockById(ev.detail.$closestBlockly.getAttribute('data-id'))
-
-      const dragger = new Blockly.BlockDragger($block, this.blockly)
-      dragger.startBlockDrag({
+      this.handsfree.dragger = new Blockly.BlockDragger($block, this.blockly)
+      this.handsfree.dragger.startBlockDrag({
         x: 0,
         y: 0
       })
+      this.handsfree.orig = {
+        x: this.$handsfree.weboji.data.pointer.x,
+        y: this.$handsfree.weboji.data.pointer.y
+      }
+    },
+
+    /**
+     * Update drag if dragging
+     */
+    onHandsfree (data) {
+      if (this.handsfree.dragger) {
+        this.handsfree.dragger.dragBlock(data, {
+          x: this.$handsfree.weboji.data.pointer.x - this.handsfree.orig.x,
+          y: this.$handsfree.weboji.data.pointer.y - this.handsfree.orig.y
+        })
+        console.log('onHandsfree', data)
+      }
     },
 
     /**
